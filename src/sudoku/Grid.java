2a4b93409481533636a4668cf87319cdafa876ea
julien <thomas.julien@protonmail.com>
2020-10-10 22:49:00 +0100

    grid init

--- src/sudoku/Grid.java
+++ src/sudoku/Grid.java
@@ -1,10 +1,16 @@
 package sudoku;
 
 public class Grid {
-    private int[][] grid = {{}};
+    private char[][] grid = {{}};
 
     public Grid() {
     }
 
-    
+    public void initGrid() {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                this.grid[i][j] = ' ';
+            }
+        }
+    }
 }
